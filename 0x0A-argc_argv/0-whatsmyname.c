687af42a13a560272605d234917d77161828ee29
Beckyshee <wanjirubecky.rw@gmail.com>
2023-04-10 14:19:05 -0700

    print its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+/**
+ * main - prints its name followed by a \n.
+ * @argc: no of arguments.
+ * @argv: array - arguments.
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
