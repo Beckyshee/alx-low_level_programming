2c4fcba4aa47e738b2f3a9e6004ee1fa15ac4df2
Beckyshee <wanjirubecky.rw@gmail.com>
2023-04-11 15:08:36 -0700

    req error fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,7 +4,7 @@
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: takes in width of grid
  * @height: height of grid
- * Returrn: grid
+ * Return: gridst.
  */
 int **alloc_grid(int width, int height)
 {
