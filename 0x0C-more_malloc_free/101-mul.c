0ad16be0e619c7071de4f2d5fd606a41b7e42c37
Beckyshee <wanjirubecky.rw@gmail.com>
2023-04-13 16:22:48 -0700

    this task needs a pld

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _is_zero - determines if a number is zero
+ * @argv: argument vector.
+ * Return: 0.
+ */
+
+void _is_zero(char *argv[])
+{
+	/*how do i even accomplish this task?????*/
+}
